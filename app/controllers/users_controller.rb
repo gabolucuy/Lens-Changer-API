1895e14c360143216ab0cef7399d28543e1ec697
sam_vs6 <samuel-vs6@hotmail.com>
2017-10-14 13:12:19 -0400

    Eliminando gemas inutiles.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   skip_before_action :authorize_request, only: :create
-  #:verify_authenticity_token
   # POST /signup
   # return authenticated token upon signup
   def create

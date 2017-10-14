fe87c71f85248d671f8c654597aab385d0f0b8aa
¨JeysonMO¨ <jeyson.mirabal@gmail.com>
2017-10-14 17:22:19 -0400

    I4.7 - Arreglos en la BD

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20171014201417) do
 
-  create_table "children", force: :cascade do |t|
-    t.string "name"
-    t.string "gender"
-    t.date "birthday"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "lagging_skills", force: :cascade do |t|
     t.text "description"
     t.integer "checked"

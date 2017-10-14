fb73f782b405170ddc5040cba0bcf0eefe1b3585
DharaRioja <cuatita_16@hotmail.com>
2017-10-14 19:39:27 -0400

    migraciones y conflicto resuelto con child

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171014201417) do
+ActiveRecord::Schema.define(version: 20171014220956) do
+
+  create_table "children", force: :cascade do |t|
+    t.string "name"
+    t.string "gender"
+    t.date "birthday"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "lagging_skills", force: :cascade do |t|
     t.text "description"

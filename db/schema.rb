a059e9e0e353de5bc460fc5b0d8360dd2975bf86
Daniel Illanes <dany.gaara.ds@gmail.com>
2017-10-14 18:30:41 -0400

    arreglo en las migraciones de childs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012001645) do
+ActiveRecord::Schema.define(version: 20171014220956) do
 
   create_table "children", force: :cascade do |t|
     t.string "name"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20171012001645) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "last_name"

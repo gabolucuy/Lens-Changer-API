7a7ee7880888077308fab4a31418bd85659f72dd
DharaRioja <cuatita_16@hotmail.com>
2017-10-14 16:32:22 -0400

    logica controlador

--- app/controllers/lagging_skill_controller.rb
+++ app/controllers/lagging_skill_controller.rb
@@ -1,2 +1,27 @@
 class LaggingSkillController < ApplicationController
+    skip_before_action :authorize_request, only: [:create, :getLaggingSkill]
+
+    def create
+        lagging_skill = LaggingSkill.create(lagging_skill_params)        
+        response = { message: "Dificultad agregada!" }
+        json_response(response)
+    end
+
+    
+    def getLaggingSkill
+        @lagging_skill = LaggingSkill.where(id: params[:lagging_skill_id])        
+        json_response(@lagging_skill)
+    end
+
+    private
+
+    def lagging_skill_params
+        params.permit(
+            :id
+            :description,
+            :checked,
+            :child_id,            
+            )
+    end    
+
 end

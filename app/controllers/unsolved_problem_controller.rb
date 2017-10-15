4c2f54a1f456250cc24f32c05db8006d8b70f633
zurich <estebanzurita316@gmail.com>
2017-10-15 17:39:12 -0400

    Controlador unsolved_problem

---
+++ app/controllers/unsolved_problem_controller.rb
@@ -0,0 +1,18 @@
+class UnsolvedProblemController < ApplicationController
+
+	def create
+    unsolved_problem = UnsolvedProblem.create!(unsolved_problem_params)
+
+
+  	end
+
+
+	def unsolved_problem_params
+    params.permit(
+    :solved,
+    :unsolved_order,
+    :unsolved_score,
+    :user_id
+    )
+  end
+end

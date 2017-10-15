94325efc5b04f35db72c332818938858526daf6b
gabolucuy <gabolucuy@hotmail.com>
2017-10-15 19:43:34 -0400

    agregando factories para unsolved problems y pruebas para el modelo

--- app/models/unsolved_problem.rb
+++ app/models/unsolved_problem.rb
@@ -1,3 +1,4 @@
 class UnsolvedProblem < ApplicationRecord
 	belongs_to :user
+	validates_presence_of  :description,:solved ,:unsolved_order ,:unsolved_score ,:user
 end
--- spec/models/unsolved_problem_spec.rb
+++ spec/models/unsolved_problem_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe UnsolvedProblem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should belong_to :user }
+
+
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:solved) }
+  it { should validate_presence_of(:unsolved_order) }
+  it { should validate_presence_of(:unsolved_score) }
+  it { should validate_presence_of(:user) }
+  
 end

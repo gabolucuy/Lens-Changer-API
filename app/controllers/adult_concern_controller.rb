f60214a95116b062e72f4562b6cc387b70dbb1af
DharaRioja <cuatita_16@hotmail.com>
2017-10-16 00:02:03 -0400

    crea y optines adults concerns

--- app/controllers/adult_concern_controller.rb
+++ app/controllers/adult_concern_controller.rb
@@ -1,3 +1,24 @@
 class AdultConcernController < ApplicationController
-
+    skip_before_action :authorize_request, only: [:create, :getAdultConcern]
+    
+        def create
+            adult_concern= AdultConcern.create(adult_concern_params)        
+            response = { message: "Adult Concern agregado!" }
+            json_response(response)
+        end
+    
+        
+        def getAdultConcern
+            @adult_concern = AdultConcern.where(id: params[:adult_concern_id])        
+            json_response(@adult_concern)
+        end
+    
+        private
+    
+        def adult_concern_params
+            params.permit(
+                :description,
+                :unsolved_problem_id,            
+                )
+        end    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
   get 'me', to: 'users#me'
+
+
+
+
+  post 'createAdultConcern', to: 'adult_concern#create'
+  get 'getAdultConcern' => 'adult_concern#getAdultConcern'
 end

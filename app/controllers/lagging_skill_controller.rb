32e814bad48fc7feb61a35465353b702cd270efd
¨JeysonMO¨ <jeyson.mirabal@gmail.com>
2017-10-16 13:41:32 -0400

    I4.7- guarda informacion de laggingSkills usando autenticacion

--- app/controllers/lagging_skill_controller.rb
+++ app/controllers/lagging_skill_controller.rb
@@ -2,16 +2,20 @@ class LaggingSkillController < ApplicationController
     skip_before_action :authorize_request, only: [:create, :getLaggingSkill]
 
     def create
-        lagging_skill = LaggingSkill.create(lagging_skill_params)        
+        lagging_skill = LaggingSkill.create(lagging_skill_params)
         response = { message: "Dificultad agregada!" }
         json_response(response)
     end
 
-    
+
     def getLaggingSkill
-        @lagging_skill = LaggingSkill.where(id: params[:lagging_skill_id])        
+        @lagging_skill = LaggingSkill.where(id: params[:lagging_skill_id])
         json_response(@lagging_skill)
     end
+
+    def protect_against_forgery?
+        false
+      end
 
     private
 
@@ -19,8 +23,8 @@ class LaggingSkillController < ApplicationController
         params.permit(
             :description,
             :checked,
-            :child_id,            
+            :child_id,
             )
-    end    
+    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171016041420) do
+ActiveRecord::Schema.define(version: 20171016051512) do
 
   create_table "adult_concerns", force: :cascade do |t|
     t.text "description"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20171016041420) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_children_on_user_id"
+  end
+
   create_table "lagging_skills", force: :cascade do |t|
     t.text "description"
     t.integer "checked"

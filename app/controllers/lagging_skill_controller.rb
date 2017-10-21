16f073ef1d5df03eb3edaedc31240f177cfc6511
¨JeysonMO¨ <jeyson.mirabal@gmail.com>
2017-10-21 12:22:33 -0400

    WIP: cambio de formato de envío para laggingSkill

--- app/controllers/lagging_skill_controller.rb
+++ app/controllers/lagging_skill_controller.rb
@@ -1,22 +1,43 @@
 class LaggingSkillController < ApplicationController
     skip_before_action :authorize_request, only: [:create, :getLaggingSkill]
 
+    # def create
+    #     LaggingSkill.destroy_all
+    #     if(LaggingSkill.exists?(params[:id]))
+    #         LaggingSkill.update(:id => params[:id], :description => params[:description], :checked => params[:checked], :child_id => params[:child_id])
+    #         laggingSkill = LaggingSkill.where(id: params[:lagging_skill_id])
+    #         #laggingSkillUncheck(laggingSkill)
+    #
+    #         response = { message: "Dificultad actualizada!"}
+    #         json_response(response)
+    #     else
+    #       laggingSkill = LaggingSkill.create(:id => params[:id], :description => params[:description], :checked => params[:checked], :child_id => params[:child_id])
+    #       response = { message: "Dificultad agregada!"}
+    #       json_response(response)
+    #     end
+    # end
+
     def create
-        if(LaggingSkill.exists?(params[:id]))
-            LaggingSkill.update(:id => params[:id], :description => params[:description], :checked => params[:checked], :child_id => params[:child_id])
-            response = { message: "Dificultad actualizada!"}
-            json_response(response)
-        else
-          laggingSkill = LaggingSkill.create(:id => params[:id], :description => params[:description], :checked => params[:checked], :child_id => params[:child_id])
-          response = { message: "Dificultad agregada!"}
-          json_response(response)
-        end
+      laggingSkillData = params[:data]
+
+      laggingSkillData.each do |laggingSkill|
+        laggingSkill = LaggingSkill.create(:id => params[:id], :description => params[:description], :checked => params[:checked], :child_id => params[:child_id])
+      end
+      response = { message: "LaggingSkills agregada!"}
+      json_response(response)
     end
 
 
     def getLaggingSkill
         @lagging_skill = LaggingSkill.where(id: params[:lagging_skill_id])
         json_response(@lagging_skill)
+    end
+
+    def destroy
+        @lagging_skill = LaggingSkill.where(id: params[:lagging_skill_id])
+        @lagging_skill.destroy
+        response = { message: "LaggingSkill Unchecked!"}
+        json_response(response)
     end
 
     def protect_against_forgery?
--- app/models/lagging_skill.rb
+++ app/models/lagging_skill.rb
@@ -1,3 +1,7 @@
 class LaggingSkill < ApplicationRecord
   belongs_to :child
+
+  def laggingSkillUncheck(laggingSkill)
+    
+  end
 end

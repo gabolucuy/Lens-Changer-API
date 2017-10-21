e52c0388b8a65553832fa53cd3f95435f05a66e9
zurich <estebanzurita316@gmail.com>
2017-10-20 21:25:42 -0400

    Recive unsolved problem con ID's de child y user correctos

--- app/controllers/unsolved_problem_controller.rb
+++ app/controllers/unsolved_problem_controller.rb
@@ -2,10 +2,21 @@ class UnsolvedProblemController < ApplicationController
     skip_before_action :authorize_request, only: [:create, :getUnsolvedProblem]
 
     	def create
-            unsolved_problem = UnsolvedProblem.create(unsolved_problem_params)
-
-            response = { message: "Unsolved Problem created"}
-            json_response(response)
+            unsolved_problem = UnsolvedProblem.new(unsolved_problem_params)
+            api_child = Child.where(child_id: unsolved_problem_params[:child_id])     
+            if api_child.count < 1
+                response = { message: "Error, child is not in database"}
+                json_response(response)
+            else
+                unsolved_problem.child_id =  api_child.first.id 
+                if unsolved_problem.save
+                    response = { message: "Unsolved Problem created"}       
+                    json_response(response)
+                else
+                    response = { message: "Error, no Unsolved Problem created"}
+                    json_response(response)
+                end            
+            end          
       	end
 
         def getUnsolvedProblem

2e2dddcbc5739b5e165ca260a155f4a1a74c5f72
Luis Veizaga <luisnlbs4@gmail.com>
2017-10-21 06:58:57 -0400

    dependencias y funciones

--- app/controllers/child_concern_controller.rb
+++ app/controllers/child_concern_controller.rb
@@ -1,2 +1,25 @@
 class ChildConcernController < ApplicationController
+  skip_before_action :authorize_request, only: [:create, :getAdultConcern]
+
+      def create
+          child_concern= ChildConcern.create(adult_concern_params)
+          response = { message: "Child concern agregado!" }
+          json_response(response)
+      end
+
+
+      def getAdultConcern
+          @child_concern = ChildConcern.where(id: params[:adult_concern_id])
+          json_response(@child_concern)
+      end
+
+      private
+
+      def adult_concern_params
+          params.permit(
+              :description,
+              :unsolved_problem_id,
+              )
+      end
+
 end

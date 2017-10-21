ada34e1162ad9766bdcb22c87d2504528284ca1f
gabolucuy <gabolucuy@hotmail.com>
2017-10-21 01:27:52 -0400

    comunicacion con app para subir a api unsolved problems

--- app/controllers/unsolved_problem_controller.rb
+++ app/controllers/unsolved_problem_controller.rb
@@ -2,27 +2,42 @@ class UnsolvedProblemController < ApplicationController
     skip_before_action :authorize_request, only: [:create, :getUnsolvedProblem]
 
     	def create
-            unsolved_problem = UnsolvedProblem.new(unsolved_problem_params)
-            api_child = Child.where(child_id: unsolved_problem_params[:child_id])     
-            if api_child.count < 1
-                response = { message: "Error, child is not in database"}
-                json_response(response)
-            else
-                unsolved_problem.child_id =  api_child.first.id 
-                if unsolved_problem.save
-                    response = { message: "Unsolved Problem created"}       
-                    json_response(response)
-                else
-                    response = { message: "Error, no Unsolved Problem created"}
-                    json_response(response)
-                end            
-            end          
+            data =  JSON.parse(params[:data])
+            response = ""
+            data.each do |json_up|
+                puts "************************"
+                puts data
+                response = create_unsolved_problem(json_up,params[:user_id])
+            end
+            json_response(response)
       	end
+        def create_unsolved_problem(json_up,user_id)
+            unsolved_problem = UnsolvedProblem.new(:description => json_up["description"],
+                                                   :solved => json_up["solved"],
+                                                   :unsolved_problem_id_app => json_up["id"],
+                                                   :unsolved_order => json_up["unsolved_order"],
+                                                   :unsolved_score => json_up["unsolved_score"],
+                                                   :user_id => user_id,
+                                                   :child_id => json_up["child_id"],
+                                                    )
 
+                api_child = UnsolvedProblem.where("child_id = ? AND user_id = ? AND unsolved_problem_id_app = ?" ,json_up["child_id"], user_id, json_up["id"]).count
+                if api_child<1
+                    if unsolved_problem.save
+                        response = { message: "Unsolved Problem created"}
+                    else
+                        response = { message: "Error, no Unsolved Problem created"}
+                    end
+                else
+                    response = {message: "Error, child is not in database"}
+                end
+
+            return response
+      	end
         def getUnsolvedProblem
             @unsolved_problem = UnsolvedProblem.where(id: params[:unsolved_problem_id])
             json_response(@unsolved_problem)
-            
+
         end
 
         def protect_against_forgery?
@@ -38,7 +53,8 @@ class UnsolvedProblemController < ApplicationController
             :unsolved_order,
             :unsolved_score,
             :user_id,
-            :child_id
+            :child_id,
+            :data
                 )
         end
 end

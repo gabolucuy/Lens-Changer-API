e750f037f2aefca630a2cf6f41be53636d2e8d4b
Luis Veizaga <luisnlbs4@gmail.com>
2017-10-22 08:53:20 -0400

    arreglando metodos

--- app/controllers/child_concern_controller.rb
+++ app/controllers/child_concern_controller.rb
@@ -1,18 +1,47 @@
 class ChildConcernController < ApplicationController
-  skip_before_action :authorize_request, only: [:create, :getChildConcern]
+  skip_before_action :authorize_request, only: [:create, :getChildConcern, :index]
 
-      def create
-          if(ChildConcern.exists?(params[:id]))
-              ChildConcern.update(:id => params[:id], :description => params[:description], :order => params[:order], :unsolved_problem_id => params[:unsolved_problem_id])
-              response = { message: "Preocupacion actualizada!"}
-              json_response(response)
-          else
-            child_concern = ChildConcern.create(:id => params[:id], :description => params[:description], :order => params[:order], :unsolved_problem_id => params[:unsolved_problem_id])
-            response = { message: "Preocupacion agregada!"}
-            json_response(response)
-          end
+      def index
+        @child_concerns = ChildConcern.where("unsolved_problem_id = ?" ,params[:unsolved_problem_id])
+        json_response(@child_concerns)
       end
 
+      def create
+        data =  JSON.parse(params[:data])
+        response = ""
+        data.each do |json_up|
+            response = create_child_concern(json_up)
+        end
+        json_response(response)
+      end
+
+      def create_child_concern(json_up)
+          child_concern = ChildConcern.new( :id => json_up["id"],
+                                            :description => json_up["description"],
+                                            :order => json_up["unsolved_order"],
+                                            :unsolved_problem_id => json_up["unsolved_problem_id"])
+              api_child = Child.where("child_id = ? AND user_id = ?",params[:child_id], params[:user_id] ).first
+              api_child_concern = ChildConcern.where("id = ? AND unsolved_problem_id = ?" ,json_up["id"], json_up["unsolved_problem_id"])
+              if api_child_concern.exists?
+                   if api_child_concern.update(:id => json_up["id"],
+                                               :description => json_up["description"],
+                                               :order => json_up["unsolved_order"],
+                                               :unsolved_problem_id => json_up["unsolved_problem_id"])
+                       response = { message: "Child Concern updated"}
+                   else
+                       response = { message: "Error, Child Concern created"}
+                   end
+              else
+                  if adult_concern.save
+                      response = { message: "Child Concern created"}
+                  else
+                      response = { message: "Error, no Child Concern created"}
+                  end
+              end
+
+
+          return response
+      end
 
       def getChildConcern
           @child_concern = ChildConcern.where(id: params[:child_concern_id])
@@ -21,16 +50,5 @@ class ChildConcernController < ApplicationController
 
       def protect_against_forgery?
           false
-        end
-
-      private
-
-      def child_concern_params
-          params.permit(
-              :description,
-              :order,
-              :unsolved_problem_id,
-              )
       end
-
 end

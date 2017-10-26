da601c4fc7bb4f0a2cd66b77fa303fe18cc761a1
zurich <estebanzurita316@gmail.com>
2017-10-26 01:55:59 -0400

    Ya puede subir y actualizar posible_solution

--- app/controllers/posible_solution_controller.rb
+++ app/controllers/posible_solution_controller.rb
@@ -11,58 +11,45 @@ class PosibleSolutionController < ApplicationController
         user_id = params[:user_id]
         child_id = params[:child_id]
         unsolved_problem_id = params[:unsolved_problem_id]
-        puts "**********SOLUTIONS***********"
-        puts data
-        puts "Usuario:" +user_id
-        puts "CHild: "+child_id
-        puts "UP: "+unsolved_problem_id
+        response = ""
         data.each do |json_up|
-            response = create_posible_solution(json_up,child_id,user_id,unsolved_problem_id)
+            response = create_posible_solution(json_up,user_id,child_id,unsolved_problem_id)
         end
         json_response(response)
       end
 
-      def create_posible_solution(json_up,child_id,user_id,unsolved_problem_id)
+      def create_posible_solution(json_up,user_id,child_id,unsolved_problem_id)
           puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@--------------@@@@@@@@@@@@@@@"
-          unsolved_problem = User.find(user_id).children.find_by_child_id(child_id).unsolved_problems.find_by_unsolved_problem_id_app(unsolved_problem_id)
-          puts "CREATE:" + unsolved_problem.id
+          api_up = User.find(user_id).children.find_by_child_id(child_id).unsolved_problems.find_by_unsolved_problem_id_app(unsolved_problem_id)
+          puts "CREATE:" + api_up.description          
+          
           solution_id = json_up["id"]
           solution_description = json_up["description"]
           solution_raitng = json_up["rating"]
           solution_unsolved_problem_id =json_up["unsolved_problem_id"]
 
-          puts solution_id 
-          puts solution_description 
-          puts solution_raitng 
-          puts solution_unsolved_problem_id
-          posible_solution = PosibleSolution.new( 
-                                            :unsolvedProblem_id => json_up["unsolvedProblem_id"],
-                                            :childConcern_id => json_up["childConcern_id"],
-                                            :adultConcern_id => json_up["adultConcern_id"],
-                                            :posibleSolution => json_up["posibleSolution"])
-              api_child = Child.where("child_id = ? AND user_id = ?",params[:child_id], params[:user_id] ).first
-              api_posible_solution = PosibleSolution.where("id = ? AND user_id = ? AND child_id= ? AND unsolvedProblem_id = ?" ,json_up["id"], json_up["user_id"], json_up["child_id"], json_up["unsolvedProblem_id"])
-              if api_posible_solution.exists?
-                   if api_posible_solution.update(:id => json_up["id"],
-                                                     :user_id => json_up["user_id"],
-                                                     :child_id => json_up["child_id"],
-                                                     :unsolvedProblem_id => json_up["unsolvedProblem_id"],
-                                                     :childConcern_id => json_up["childConcern_id"],
-                                                     :adultConcern_id => json_up["adultConcern_id"],
-                                                     :posibleSolution => json_up["posibleSolution"])
-                       response = { message: "Posible Solution updated successfully"}
-                   else
-                       response = { message: "Error, Posible Solution was not updated successfully"}
-                   end
+          unsolved_problems_api = User.find(user_id).children.find_by_child_id(child_id).unsolved_problems.find_by_unsolved_problem_id_app(unsolved_problem_id)
+          api_posible_solution = unsolved_problems_api.posible_solutions.find_by_posible_solution_id(solution_id)
+          if api_posible_solution != nil
+              if api_posible_solution.update(
+                    :rating => solution_raitng,
+                    :description => solution_description)
+                    response = { message: "Posible Solution updated successfully"}
               else
-                  if posible_solution.save
-                      response = { message: "Posible Solution created successfully"}
-                  else
-                      response = { message: "Error, Posible Solution was not created successfully"}
-                  end
+                    response = { message: "Error, Posible Solution was not updated successfully"}
               end
-
-
+          else
+              posible_solution = PosibleSolution.new( 
+                  :posible_solution_id => solution_id,
+                  :rating => solution_raitng,
+                  :description => solution_description,
+                  :unsolved_problem_id => unsolved_problems_api.id)
+              if posible_solution.save
+                response = { message: "Posible Solution created successfully"}
+                else
+                  response = { message: "Error, Posible Solution was not created successfully"}
+                end
+              end
           return response
       end
 

fa2dbbb3626600ec7e17b9314d9cec523bdf3482
sam_vs6 <samuel-vs6@hotmail.com>
2017-10-26 20:27:32 -0400

    I5.1 - Index para enlistar todos los posible solucions segun los parametros que se envian

--- app/controllers/posible_solution_controller.rb
+++ app/controllers/posible_solution_controller.rb
@@ -1,8 +1,8 @@
 class PosibleSolutionController < ApplicationController
-  skip_before_action :authorize_request, only: [:create, :getChildConcern, :index]
+  skip_before_action :authorize_request, only: [:create, :getPosibleSolutions, :index]
 
       def index
-        @posible_solutions = PosibleSolution.where("user_id = ?, unsolved_problem_id = ?" ,params[:user_id, :unsolved_problem_id])
+        @posible_solutions= User.find(params[:user_id]).children.find_by_child_id(params[:child_id]).unsolved_problems.find_by_unsolved_problem_id_app(params[:unsolved_problem_id]).posible_solutions
         json_response(@posible_solutions)
       end
 
@@ -19,8 +19,8 @@ class PosibleSolutionController < ApplicationController
       end
 
       def create_posible_solution(json_up,user_id,child_id,unsolved_problem_id)
-          api_up = User.find(user_id).children.find_by_child_id(child_id).unsolved_problems.find_by_unsolved_problem_id_app(unsolved_problem_id)        
-          
+          api_up = User.find(user_id).children.find_by_child_id(child_id).unsolved_problems.find_by_unsolved_problem_id_app(unsolved_problem_id)
+
           solution_id = json_up["id"]
           solution_description = json_up["description"]
           solution_raitng = json_up["rating"]
@@ -28,7 +28,7 @@ class PosibleSolutionController < ApplicationController
 
           unsolved_problems_api = User.find(user_id).children.find_by_child_id(child_id).unsolved_problems.find_by_unsolved_problem_id_app(unsolved_problem_id)
           api_posible_solution = unsolved_problems_api.posible_solutions.find_by_posible_solution_id(solution_id)
-          
+
           if api_posible_solution != nil
               if api_posible_solution.update(
                     :rating => solution_raitng,
@@ -38,12 +38,12 @@ class PosibleSolutionController < ApplicationController
                     response = { message: "Error, Posible Solution was not updated successfully"}
               end
           else
-              posible_solution = PosibleSolution.new( 
+              posible_solution = PosibleSolution.new(
                   :posible_solution_id => solution_id,
                   :rating => solution_raitng,
                   :description => solution_description,
                   :unsolved_problem_id => unsolved_problems_api.id)
-              
+
               if posible_solution.save
                 response = { message: "Posible Solution created successfully"}
                 else
@@ -51,11 +51,6 @@ class PosibleSolutionController < ApplicationController
                 end
               end
           return response
-      end
-
-      def getPosibleSolution
-          @posible_solution = PosibleSolution.where(id: params[:posibleSolution_id])
-          json_response(@posible_solution)
       end
       def protect_against_forgery?
           false

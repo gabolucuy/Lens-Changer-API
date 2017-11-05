a47205eb6d0efea6e5bdb2fab3a76ea3e9bec931
¨JeysonMO¨ <jeyson.mirabal@gmail.com>
2017-11-05 17:40:29 -0400

    nuevo arreglo para contacs

--- db/migrate/20171025012207_create_contacts.rb
+++ db/migrate/20171025012207_create_contacts.rb
@@ -1,7 +1,7 @@
 class CreateContacts < ActiveRecord::Migration[5.1]
   def change
     create_table :contacts do |t|
-      add_foreign_key :contacts, :users, column: :friend_id
+      #add_foreign_key :contacts, :users, column: :friend_id
       #t.references :friend
       t.references :user, foreign_key: true
 

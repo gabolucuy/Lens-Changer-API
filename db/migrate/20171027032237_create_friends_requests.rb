2a8eeca4d5853ec2cf9d752fd1b02ecb1e124bdd
¨JeysonMO¨ <jeyson.mirabal@gmail.com>
2017-11-05 18:12:30 -0400

    comentando

--- db/migrate/20171027032237_create_friends_requests.rb
+++ db/migrate/20171027032237_create_friends_requests.rb
@@ -3,10 +3,10 @@ class CreateFriendsRequests < ActiveRecord::Migration[5.1]
     create_table :friends_requests do |t|
 
       t.references :user, foreign_key: true
-      #t.references :applicant
+      t.references :applicant
 
       t.timestamps
     end
   end
-  add_foreign_key :friends_requests, :users, column: :applicant_id
+  #add_foreign_key :friends_requests, :users, column: :applicant_id
 end

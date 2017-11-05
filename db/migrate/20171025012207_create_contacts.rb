29dddda12ba6731f4af293f5430e4d7baaf63700
¨JeysonMO¨ <jeyson.mirabal@gmail.com>
2017-11-05 17:38:34 -0400

    intentando reparar migraciones

--- db/migrate/20171025012207_create_contacts.rb
+++ db/migrate/20171025012207_create_contacts.rb
@@ -2,7 +2,7 @@ class CreateContacts < ActiveRecord::Migration[5.1]
   def change
     create_table :contacts do |t|
       add_foreign_key :contacts, :users, column: :friend_id
-      t.references :friend
+      #t.references :friend
       t.references :user, foreign_key: true
 
       t.timestamps

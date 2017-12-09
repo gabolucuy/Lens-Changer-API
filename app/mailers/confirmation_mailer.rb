ecaed51ef5bafe018e16d7bb98d7a394836c6e9b
Daniel Illanes <dany.gaara.ds@gmail.com>
2017-12-09 16:22:28 -0400

    Modificacion de cuenta para enviar emails

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,12 +1,12 @@
 class ConfirmationMailer < ApplicationMailer
     
-      default from: "emailparapruebas01@gmail.com"
+      default from: "livesinthebalance09@gmail.com"
       layout 'mailer'
     
       def verification_send(user)
     #  @company=company
         @user=user
-        mail(to: user.email, subject:"Verificacion")
+        mail(to: user.email, subject:"Account Verification")
         #@parameters[:companymail]
       end
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,8 +33,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'emailparapruebas01@gmail.com',
-    password:             'Holas12345',
+    user_name:            'livesinthebalance09@gmail.com',
+    password:             'LaCato2016..',
     authentication:       'plain',
     enable_starttls_auto: true  }
   
